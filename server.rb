03a6f6a1f3961f572a58aab747fe0f9b464a758e
James Brown <jbrown@invoca.com>
2016-10-27 14:59:59 -0700

    added expire endpoint

--- server.rb
+++ server.rb
@@ -8,7 +8,7 @@ security_group = ENV['GK_SGID'] || 'sg-XXX'
 auth_token = ENV['GK_AUTH_TOKEN']
 
 before do
-  error 401 unless params[:key] == auth_token
+  error 401 unless request.env['HTTP_KEY'] == auth_token
 end
 
 get '/' do
@@ -19,4 +19,9 @@ post '/whitelist/:ip' do
   user_ip = params[:ip]
   w = Whitelister.new('us-east-1', security_group)
   w.authorize_ip(user_ip)
+end
+
+post '/expire/' do
+  w = Whitelister.new('us-east-1', security_group)
+  w.expire
 end
