e691689d8ebb67fb6f6251cde908d483e4a7c027
Maximillian Brain <mbrain@nbmbrain.local>
2017-08-23 13:06:12 -0700

    Refactored scheduler.rb and added appropiate unit tests.

--- gate_keeper.rb
+++ gate_keeper.rb
@@ -16,6 +16,7 @@ class GateKeeper < Sinatra::Base
 
   def initialize
     $stdout.sync = true
+    GateKeeperScheduler.run
     super
   end
 
--- scheduler.rb
+++ scheduler.rb
@@ -1,18 +1,36 @@
 require 'rufus-scheduler'
-require_relative './whitelister.rb'
+require 'logger'
 
-scheduler = Rufus::Scheduler.new
-logger = Logger.new(STDOUT)
+require_relative './whitelister'
 
-$stdout.sync = true
+class GateKeeperScheduler
+  attr_reader :logger, :scheduler, :whitelister
 
-if !ENV['AWS_REGION'].nil? && !ENV['GK_SGID'].nil? && ENV['RACK_ENV'] != 'test'
-  logger.debug 'ENV variables present, starting scheduler.'
-  scheduler.every '2h' do
-  logger.debug 'Running whitelister.expire...'
-  whitelister = Whitelister.new(ENV['AWS_REGION'], ENV['GK_SGID'])
-  whitelister.expire
+  def self.run
+    if !ENV['AWS_REGION'].nil? && !ENV['GK_SGID'].nil? && ENV['RACK_ENV'] != 'test'
+      logger.debug 'ENV variables present, starting scheduler.'
+      scheduler.every '2h' do
+      logger.debug 'Running whitelister.expire...'
+      whitelister = Whitelister.new(ENV['AWS_REGION'], ENV['GK_SGID'])
+      whitelister.expire
+    end
+    else
+      logger.warn 'Missing ENV variables or you are in a test environment. Not running the scheduler'
+    end
   end
-else
-  logger.warn 'Missing ENV variables or in test environment. Not running the scheduler'
+
+  private
+
+  def self.scheduler
+    @scheduler ||= Rufus::Scheduler.new
+  end
+
+  def self.logger
+    $stdout.sync = true
+    @logger ||= Logger.new(STDOUT)
+  end
+
+  def self.whitelister
+    @whitelister ||= Whitelister.new(ENV['AWS_REGION'], ENV['GK_SGID'])
+  end
 end
